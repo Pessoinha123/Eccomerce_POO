a359e23a74300758c650c8281ec49007ccc3825d
Larissarff <larissa.rfferreira4800@gmail.com>
2025-10-27 17:24:44 -0300

    feat: database configuration on project and structure of database of ProdutoEntity

---
+++ src/AppEcommerce.Infra.Data/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+using AppEcommerce.Domain.Entities;
+using AppEcommerce.Infra.Data.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppEcommerce.Infra.Data.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+    public DbSet<ProdutoEntity> ProdutoEntitys => Set<ProdutoEntity>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ProdutoConfig());
+        base.OnModelCreating(modelBuilder);
+    }
+}
